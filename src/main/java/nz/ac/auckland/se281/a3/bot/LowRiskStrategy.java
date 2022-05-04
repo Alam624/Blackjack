dac55e86c0fcea723e649cc8b26628b24236ac60
Alam624 <alam624@aucklanduni.ac.nz>
2022-05-05 00:20:43 +1200

    fixed LowRisk Strategy

--- src/main/java/nz/ac/auckland/se281/a3/bot/LowRiskStrategy.java
+++ src/main/java/nz/ac/auckland/se281/a3/bot/LowRiskStrategy.java
@@ -1,19 +1,27 @@
 package nz.ac.auckland.se281.a3.bot;
 
+import java.util.Random;
+
 import nz.ac.auckland.se281.a3.Hand;
+import nz.ac.auckland.se281.a3.Participant.Action;
 
 public class LowRiskStrategy implements Strategy {
 
+	Random rand = new Random();
+
 	@Override
 	public nz.ac.auckland.se281.a3.Participant.Action Action(Hand hand) {
+		if (hand.getScore() >= 17) {
+			return Action.HOLD;
+		} else {
+			return Action.HIT;
+		}
 
-		return null;
 	}
 
 	@Override
 	public int Bet() {
-
-		return 0;
+		return (rand.nextInt(30) + 1);
 	}
 
 }
