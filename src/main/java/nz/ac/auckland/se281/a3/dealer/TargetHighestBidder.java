4002d697772d22b37d97d02d7a2111dc611c02d4
Alam624 <alam624@aucklanduni.ac.nz>
2022-05-08 18:45:08 +1200

    fixed holding when same score and when target has blackjack

--- src/main/java/nz/ac/auckland/se281/a3/dealer/TargetHighestBidder.java
+++ src/main/java/nz/ac/auckland/se281/a3/dealer/TargetHighestBidder.java
@@ -19,10 +19,18 @@ public class TargetHighestBidder implements DealerStrategy {
 	public Action Action(Hand dealerHand) {
 
 		Player target = getMaxBidder(players.get(0), players.get(1), players.get(2));
-		if (dealerHand.getScore() > target.getHand().getScore()) {
-			return Action.HOLD;
+		if (!target.getHand().isBlackJack()) {
+			if (dealerHand.getScore() >= target.getHand().getScore()) {
+				return Action.HOLD;
+			} else {
+				return Action.HIT;
+			}
 		} else {
-			return Action.HIT;
+			if (dealerHand.getScore() >= 17) {
+				return Action.HOLD;
+			} else {
+				return Action.HIT;
+			}
 		}
 	}
 
