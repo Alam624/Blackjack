851ff01a1ee3367b20ccc27632d54d12405ad68d
Alam624 <alam624@aucklanduni.ac.nz>
2022-05-14 18:28:29 +1200

    added Javadoc and comments to StrategyFactory class

--- src/main/java/nz/ac/auckland/se281/a3/bot/StrategyFactory.java
+++ src/main/java/nz/ac/auckland/se281/a3/bot/StrategyFactory.java
@@ -2,7 +2,16 @@ package nz.ac.auckland.se281.a3.bot;
 
 public class StrategyFactory {
 
+	/**
+	 * Static method used to create bot strategies based on a string input. Uses
+	 * switch case to select the strategy.
+	 * 
+	 * @param type String input to choose the strategy
+	 * @return the selected Strategy object or null if the input type is invalid
+	 */
 	public static Strategy createStrategy(String type) {
+
+		// checks contents of type and creates strategy based on results
 		switch (type) {
 		case "R":
 			return new BotRandomStrategy();
