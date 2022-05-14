201ab5e8488c15db0d5f9db0507f1875b84704dd
Alam624 <alam624@aucklanduni.ac.nz>
2022-05-14 17:42:14 +1200

    fixed ordering

--- src/main/java/nz/ac/auckland/se281/a3/Player.java
+++ src/main/java/nz/ac/auckland/se281/a3/Player.java
@@ -7,6 +7,10 @@ package nz.ac.auckland.se281.a3;
  */
 public abstract class Player extends Participant {
 
+	public Player(String name) {
+		super(name);
+	}
+
 	private int wins = 0;
 	private int losses = 0;
 
@@ -28,10 +32,6 @@ public abstract class Player extends Participant {
 
 	public int getLosses() {
 		return losses;
-	}
-
-	public Player(String name) {
-		super(name);
 	}
 
 	public abstract int makeABet();
