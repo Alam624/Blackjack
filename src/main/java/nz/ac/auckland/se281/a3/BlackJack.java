e641a7b9591335c0a57818a4d454b34d99efe7c2
Alam624 <alam624@aucklanduni.ac.nz>
2022-05-14 18:10:19 +1200

    added comments and Javadocs to the BlackJack class

--- src/main/java/nz/ac/auckland/se281/a3/BlackJack.java
+++ src/main/java/nz/ac/auckland/se281/a3/BlackJack.java
@@ -83,13 +83,18 @@ public class BlackJack {
 	 * Task1
 	 */
 	protected void initBots() {
+
+		// creates bot objects
 		Bot bot1 = new Bot("Bot1");
 		Bot bot2 = new Bot("Bot2");
+
 		String botStrategyString = getBotStrategy();
 		// create and set Bots strategy here
 		Strategy botStrategy = StrategyFactory.createStrategy(botStrategyString);
 		bot1.setStrategy(botStrategy);
 		bot2.setStrategy(botStrategy);
+
+		// add the bots to the list of players
 		players.add(bot1);
 		players.add(bot2);
 	}
@@ -110,11 +115,15 @@ public class BlackJack {
 	 */
 	protected void printAndUpdateResults(int round) {
 
+		// selects the top winner
 		Player netWinner = getNetWinner();
 
+		// prints the round results
 		System.out.println(winLoss(players.get(0), round));
 		System.out.println(winLoss(players.get(1), round));
 		System.out.println(winLoss(players.get(2), round));
+
+		// checks if the dealer needs to change strategies
 		if (netWinner.getNetWins() >= 2) {
 			dealer.setStrategy(new TargetTopWinner(this));
 		} else {
@@ -123,14 +132,24 @@ public class BlackJack {
 
 	}
 
+	/**
+	 * compares the net wins of the three players and returns the Player object of
+	 * the one with the highest number of net wins.
+	 * 
+	 * @return the Player with the most net wins
+	 */
 	public Player getNetWinner() {
 
+		// creates variable for the top winner
 		Player netWinner;
 
+		// checks if the second player has more wins than the first player
 		netWinner = players.get(0);
 		if (players.get(1).getNetWins() > netWinner.getNetWins()) {
 			netWinner = players.get(1);
 		}
+
+		// checks if the third player has more wins than the current highest winner
 		if (players.get(2).getNetWins() > netWinner.getNetWins()) {
 			netWinner = players.get(2);
 		}
@@ -142,25 +161,41 @@ public class BlackJack {
 	 * TODO This method should print the statistic of the game when it ends
 	 */
 	protected void printGameStatistics() {
+
+		// loops through all the players and prints each of their statistics
 		for (Player player : players) {
 			System.out.println(
 					player.getName() + " won " + player.getWins() + " times and lost " + player.getLosses() + " times");
 		}
 	}
 
+	/**
+	 * Determines if the input player won the round against the dealer or not and
+	 * returns a string describing the round results and how many chips the player
+	 * won/lost
+	 * 
+	 * @param player player that is to be compared with the dealer
+	 * @param round  current round number
+	 * @return a string describing the result of the round for the input player
+	 */
 	private String winLoss(Player player, int round) {
 
+		// checks for Dealer having blackjack or player being bust, both cases resulting
+		// in a loss.
 		if (dealer.getHand().isBlackJack() || player.getHand().isBust()) {
 			player.addLoss();
 			return "Round " + round + ": " + player.getName() + " lost " + player.getHand().getBet() + " chips";
 		}
 
+		// checks if the players score is higher than the dealers, dealer is bust or
+		// player has blackjack. If so then player wins
 		if (player.getHand().isBlackJack() || player.getHand().getScore() > dealer.getHand().getScore()
 				|| dealer.getHand().isBust()) {
 			player.addWin();
 			return "Round " + round + ": " + player.getName() + " won " + player.getHand().getBet() + " chips";
 		}
 
+		// all other cases player loses
 		player.addLoss();
 		return "Round " + round + ": " + player.getName() + " lost " + player.getHand().getBet() + " chips";
 	}
